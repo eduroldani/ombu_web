dd6a965f3dda424332523287beed742f5be2b569
eduardo roldan <eduroldanisaac@gmail.com>
2022-12-03 17:27:39 +0100

    front end

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,33 @@
 class CoursesController < ApplicationController
+
+  def index
+    if params.present?
+      @courses = Course.all
+      # @courses = Course.where(title: params[:search])
+    else
+      @courses = Course.all
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
